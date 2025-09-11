6a84face57078cc6f70b49a902dc7a5a1d4f5afc
Evan Marshall <unconfused@gmail.com>
2025-09-11 13:36:03 -0500

    my colorscheme.lua for nvim

---
+++ colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "MarcoKorinth/onehalf.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onehalfdark",
+    },
+  },
+}
